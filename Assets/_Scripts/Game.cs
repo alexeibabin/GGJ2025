1bc21a4d0a5b241bb44dfd4e6fb0a26c615bcff9
Alexei Babin <>
2025-01-23 22:16:04 +0200

    Added uniRX and base Game and EventHub classes

---
+++ Assets/_Scripts/Game.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+   public static IEventHub EventHub { get; private set; }
+   
+   // Start is called before the first frame update
+   void Start()
+   {
+       
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+       
+   }
+}
