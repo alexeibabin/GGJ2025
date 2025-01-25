962ce31a81ac02875694547c6460f7cb14f0c6fd
Nir Zaid <NirZaid6@gmail.com>
2025-01-25 16:46:51 +0200

    Link end button to end game menu

--- Assets/_Scripts/EndGameMenu.cs
+++ Assets/_Scripts/EndGameMenu.cs
@@ -1,14 +1,18 @@
-using System;
+using _Scripts.Collectables;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EndGameMenu : MonoBehaviour
 {
     [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Text text;
 
     private void Awake()
     {
         mainMenuButton.onClick.AddListener(GoToMainMenu);
+        int coinsCount = Game.SessionData.GetCollectableCount(ECollectableType.Coin);
+        
+        //text.text = $"You managed to carry {coinsCount} seeds of hope.";
     }
 
     private void GoToMainMenu()
